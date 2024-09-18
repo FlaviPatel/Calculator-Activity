ae065ff14b70ed34f65fb743161db031c240b694
Flavi Patel <divyapatel8166@gmail.com>
2024-09-18 19:06:45 +0530

    Created an activity on calculation

---
+++ main.py
@@ -0,0 +1,26 @@
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    return x // y
+
+x = int(input("Enter the first number:"))
+y = int(input("Enter the second number:"))
+
+# Find the addition
+print("Addition:", add(x, y))
+
+# Find the difference
+print("subtraction:", subtract(x, y))
+
+# Find the  product
+print("Multiplication:", multiply(x, y))
+
+# Find the Quotient
+print("Division:", divide(x, y))
+
+
+
